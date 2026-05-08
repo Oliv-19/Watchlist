c327ce85bd9be58b0121e9a5ad63bbd1137c2532
Oliv-19 <113481931+Oliv-19@users.noreply.github.com>
2026-05-08 15:43:55 -0400

    Fix meadia component to fit text

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -8,10 +8,12 @@ const BG_SIZE = "original"
 
 const LeftInfo = ({data}) => {
     return(
-        <div className="w-80 text-center flex flex-col gap-4 text-[18px] my-15">
+        <div className="w-90 text-center flex flex-col gap-4 text-[18px] mt-5">
             <h1 className="text-4xl ">{data.name}</h1>
             {data.original_name != data.name && (<h5>{data.original_name}</h5>)  }
-            <p className="text-[1rem] font-medium my-auto">{data.overview}</p>
+            <p className={`w-full text-[1rem] font-medium m-auto`}>
+                {data.overview}
+            </p>
         </div>
     )
 }
@@ -25,7 +27,7 @@ const Details = ({title, info}) => {
 }
 const RightInfo = ({data}) => {
     return(
-        <div className="w-80 self-center text-center flex flex-col gap-4 text-[1rem] items-start">
+        <div className="w-80 self-center text-center flex flex-col gap-4 text-[1rem] items-start my-25">
             <Details title='calendar' info={`${format(new Date(data.first_air_date), 'MMM d, y')} - ${format(new Date(data.last_air_date), 'MMM d, y')}`}/>
             <Details title='episodes' info={`${data.number_of_episodes} Episodes`}/>
             <Details title='episodes' info={`${data.number_of_seasons} Seasons`}/>
@@ -42,19 +44,24 @@ function Media() {
     if(data == null){
         return <div>Loading...</div>
     }
-    console.log(data);
     
     const fullImageUrl = `${IMAGE_BASE_URL}${POSTER_SIZE}${data.poster_path}`
     const fullBGImageUrl = `${IMAGE_BASE_URL}${BG_SIZE}${data.backdrop_path}`
+    
     return (
         <>
             {data && (
-                <div className="bg-black w-full h-dvh">
-                    <img className="w-full absolute z-0 top-6px opacity-20" src={fullBGImageUrl} alt="" />
-                    <div className="w-full h-dvh flex justify-evenly relative z-1 text-white">
-                        <LeftInfo data={data}/>
-                        <img className="self-center" src={fullImageUrl} alt="" />
-                        <RightInfo data={data} />
+                <div className="bg-black w-full h-full">
+                    <div className="h-fit relative">
+                        <img className={`w-full h-full object-cover absolute z-0 opacity-20`} src={fullBGImageUrl} alt="" />
+                        <div className="pt-8 pb-2 w-full h-fit flex justify-evenly relative z-1 text-white">
+                            <LeftInfo data={data}/>
+                            <img className="h-fit" src={fullImageUrl} alt="" />
+                            <RightInfo data={data} />
+
+                        </div>
+                    </div>
+                    <div className="h-100 w-full  bg-black">
 
                     </div>
                 </div>
